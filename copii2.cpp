0973b270fc03c619ae711cb3e9a74b8a878d0d4b
sophdev2011 <sophia.maria.tudor.2011@gmail.com>
2022-11-27 12:03:28 +0200

    Adaugare problema Copii2

---
+++ copii2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int f,b,z;// declaratia a 3 variabile
+    cin>>f>>b>>z;//citeste nr de fete, baieti si zile
+    // cout<<"\nFete= ";
+    // cout<<f;
+    // cout<<"\nBaieti= ";
+    // cout<<B;
+    // cout<<"\nZile";
+    // cout<<z;
+    // cout<<f+b
+    // cout<<"\nPagini= ";
+    cout<<(f*3+b*2)*z;//am afisat suma lor
+
+
+    return 0;
+}
