d18a37fc471f8579fdd5a94e19e8e1fdeeb56d27
sophdev2011 <sophia.maria.tudor.2011@gmail.com>
2022-11-27 11:46:46 +0200

    Adaugare problema globulete.

---
+++ globuri.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,r,v;// declaratia a 3 variabile
+    cin>>a;//citesc numarul de globuri albe
+    // cout<<"\nGloburi albe= ";
+    // cout<<a;
+    // cout<<"\nGloburi rosii= ";
+   
+    r=a*2;
+    // cout<<r;
+    // cout<<"\nGloburi verzi= ";
+    
+    v=r-3;
+   // cout<<v;
+   // cout<<"\nSuma globuri= ";
+    cout<<a+r+v;//am afisat suma lor
+
+
+    return 0;
+}
