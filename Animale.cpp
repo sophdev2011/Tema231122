5fc11c0dd52fca71a37a3d68c9ada2ebbf20760f
sophdev2011 <sophia.maria.tudor.2011@gmail.com>
2022-11-27 12:18:34 +0200

    Adaugare problema Animale

---
+++ Animale.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int c,p,g;
+    cin>>c;
+    // cout<<"\nCaini= ";
+    // cout<<c;
+    // cout<<"\nPisici= ";
+   
+    p=c*2;
+    // cout<<p;
+    // cout<<"\nGaini= ";
+    
+    g=p*2;
+   // cout<<g;
+   // cout<<"\nAnimale= ";
+    cout<<c+c*2+c*4;
+
+
+    return 0;
+}
