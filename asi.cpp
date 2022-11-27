9465b71fac2bc30a04cc01ba5bf7241ab7068c0e
sophdev2011 <sophia.maria.tudor.2011@gmail.com>
2022-11-27 10:47:36 +0200

    Primele mele exercitii

---
+++ asi.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a,b;// declaratia a 2 variabile
+    cout<<"Introdu nr. a= ";
+    cin>>a;
+    cout<<"\n";
+    cout<<"Introdu nr. b= ";
+    cin>>b;
+    cout<<"\n";
+
+    cout<<"Suma= ";
+    cout<<a+b;//am afisat suma lor
+    cout<<"\n";
+
+    cout<<"Diferenta= ";
+    cout<<a-b;//am afisat diferenta lor
+    cout<<"\n";
+
+    cout<<"Produsul= ";
+    cout<<a*b;//am afisat produsul lor
+    cout<<"\n";
+
+    cout<<"Impartirea= ";
+    cout<<a/b;//am afisat catul lor
+
+    return 0;
+}
---
+++ sum00.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;// declaratia a 2 variabile
+    cin>>a>>b;//citesc 2 valori in cele 2 variabile
+    cout<<a+b;//am afisat suma lor
+
+    return 0;
+}
