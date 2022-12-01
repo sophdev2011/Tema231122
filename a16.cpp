298a73244ed8f69d4fd1d7dff3ce474e9e3c56c1
sophdev2011 <sophia.maria.tudor.2011@gmail.com>
2022-12-01 15:42:41 +0200

    Adaugare problema a16

---
+++ a16.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long a;
+    cin >> a;
+    a = a * a;
+    a = a * a;
+    a = a * a;
+    a = a * a;
+    cout << a;
+    return 0;
+}
